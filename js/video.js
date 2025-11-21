4933cde3710b91d351e7b092b5d2c3a0b971e52b
alihall02 <alihall@umich.edu>
2025-11-21 13:41:12 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	video.play();
-	console.log("Play Video - volume: " + Math.round((video.volume * 100).toFixed(0)) + "%");
+	console.log("Play Video - volume: " +(video.volume * 100).toFixed(0) + "%");
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
@@ -48,7 +48,7 @@ document.querySelector("#mute").addEventListener("click", function() {
 
 document.querySelector("#slider").addEventListener("change", function() {
     video.volume = this.value / 100;
-    document.querySelector("#volume").textContent = (video.volume * 100);
+    document.querySelector("#volume").innerHTML = document.querySelector("#slider").value + "%";
     console.log("Volume changed to:", video.volume * 100 + "%");
 });
 document.querySelector("#vintage").addEventListener("click", function() {
